35bb0d7aa87234b42a1a92fe82d576f940aa961e
steepllama <steepllama@gmail.com>
2022-02-03 18:47:09 -0600

    added an attribute to exclude the password from showing up in our database

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'password', {
+const sequelize = new Sequelize('just_tech_news_db', 'root', 'Manchis1', {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
