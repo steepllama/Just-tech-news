4f34adeaa3e3f714646012ad3086d364d3390bb0
steepllama <steepllama@gmail.com>
2022-02-03 20:25:06 -0600

    added the hashing for the password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -29,31 +30,43 @@ User.init(
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            len:[4]
+            len: [4]
         }
     }
 },
 {
+    hooks: {
+    // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+    // set up beforeUpdate lifecycle "hook" functionality
+    async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+    },
+    },
     sequelize, 
     timestamps: false, 
     freezeTableName: true, 
     underscored: true, 
     modelName: 'user'
-},
-    { 
+}
+    // { 
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
+    // sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
+    // timestamps: false,
     // don't pluralize name of database table
-    freezeTableName: true,
+    // freezeTableName: true,
     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
+    // underscored: true,
     // make it so our model name stays lowercase in the database
-    modelName: "user",
-    }
+    // modelName: "user",
+    // }
 );
 
 module.exports = User;
