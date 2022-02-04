960d8d871dea72d53141e16d22b9631979946da1
steepllama <steepllama@gmail.com>
2022-02-04 10:19:22 -0600

    incorporated the async function for the password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const bcrypt = require("bcrypt");
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -48,6 +49,29 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!'});
+            return;
+        }
+
+        // res.json({ user: dbUserData });
+
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
 });
 
 // PUT /api/users/1
