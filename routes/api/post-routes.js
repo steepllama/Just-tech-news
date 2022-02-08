19adec687c5a441baab342a12462de92e7b28e9a
steepllama <steepllama@gmail.com>
2022-02-07 19:35:57 -0600

    updated the code with the snapshot

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,15 +1,23 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const { sequelize } = require("../../models/Post");
 
 // get all users
 router.get("/", (req, res) => {
   Post.findAll({
+    order: [["created_at", "DESC"]],
     attributes: ["id", "post_url", "title", "created_at",
     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count'],
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'create_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ["username"],
