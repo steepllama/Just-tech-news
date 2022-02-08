53eb591685ed725453d8cd064dc632723021787b
steepllama <steepllama@gmail.com>
2022-02-07 22:02:57 -0600

    Comment wasn't being read and I went in to fix the user-route file to make sure comment was being imported and read

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, User, Vote, Comment } = require("../../models");
+const { Post, User, Comment, Vote } = require("../../models");
 const { sequelize } = require("../../models/Post");
 
 // get all users
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 const bcrypt = require("bcrypt");
-const { User, Post, Vote } = require("../models");
+const { User, Post, Vote, Comment } = require("../models");
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -19,23 +19,31 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        include: [
-    {
-    model: Post,
-    attributes: ['id', 'title', 'post_url', 'created_at']
-    },
-    {
-    model: Post,
-    attributes: ['title'],
-    through: Vote,
-    as: 'voted_posts'
-    }
-],
         attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
-        }
-    })
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ],
+        })
         .then(dbUserData => {
             if (!dbUserData) {
                 res.status(404).json({ message: 'No user found with this id'});
