e51b48638cb056a4cd2f69ec1c3f47832eb79236
steepllama <steepllama@gmail.com>
2022-02-10 08:22:40 -0600

    added the ability for users to be able to log in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const posts = dbPostData.map(post => post.get({ plain: true }));
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -36,6 +35,23 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+// const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    // other logic
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // turn on routes
 app.use(routes);
 
