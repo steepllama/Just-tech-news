cecaf4c65d7295b5e0888a12d5f59c6816d4d019
steepllama <steepllama@gmail.com>
2022-02-11 08:15:07 -0600

    fixed the issue of the server not recalling the routes on the server file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
 
 const app = express();
@@ -30,6 +31,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
