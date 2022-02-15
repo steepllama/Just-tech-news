322018065d6c3c1f2987dbaa8e5fcadde346ede8
steepllama <steepllama@gmail.com>
2022-02-14 21:13:35 -0600

    added the feature to prevent people form double voting

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -84,12 +84,16 @@ router.post("/", (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  Post.upvote({ ...req.body, user_id: req.session.user.id }, { Vote, Comment, User })
-    .then(updatePostData => res.json(updatePostData))
+  // if make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
-    console.log(err);
-    res.status(500).json(err)
+      console.log(err);
+      res.status(500).json(err);
     });
+  }
 });
 
 router.put("/:id", (req, res) => {
